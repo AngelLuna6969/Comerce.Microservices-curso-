7f1d8788e777d4662fd385f01ad269b618a3b382
Angel Luna <angellunas2609@gmail.com>
2025-07-09 19:31:34 -0600

    Completado de la carpeta service de customers

---
+++ src/Services/Common/Customer.Service.EventHandler/ClientEventHandler.cs
@@ -0,0 +1,25 @@
+﻿using Customer.Domain;
+using Customer.Persistence.Database;
+using Customer.Service.EventHandler.Commands;
+using MediatR;
+
+namespace Customer.Service.EventHandler
+{
+    public class ClientEventHandler : INotificationHandler<ClientCreateCommand>
+    {
+        private readonly ApplicationDbContext _context;
+        public ClientEventHandler(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Handle(ClientCreateCommand notification, CancellationToken cancellationToken)
+        {
+            await _context.AddAsync(new Client
+            {
+                Name = notification.Name,
+            });
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ src/Services/Common/Customer.Service.Queries/ClientQueryService.cs
@@ -0,0 +1,37 @@
+﻿using Customer.Persistence.Database;
+using Customer.Service.Queries.DTO;
+using Service.Common.Collection;
+using Microsoft.EntityFrameworkCore;
+using Service.Common.Paging;
+using Service.Common.Mapping;
+
+namespace Customer.Service.Queries
+{
+    public interface IClientQueryService
+    {
+        Task<DataCollection<ClientDTO>> GetAllAsync(int page, int take, IEnumerable<int> clients = null);
+        Task<ClientDTO> GetAsync(int id);
+    }
+    public class ClientQueryService : IClientQueryService
+    {
+        private readonly ApplicationDbContext _context;
+        public ClientQueryService(ApplicationDbContext context) 
+        {
+            _context = context;
+        }
+
+        public async Task<DataCollection<ClientDTO>> GetAllAsync(int page, int take, IEnumerable<int> clients = null)
+        {
+            var collection = await _context.Clients
+                .Where(x => clients == null || clients.Contains(x.ClientId))
+                .OrderBy(x => x.Name)
+                .GetPagedAsync(page, take);
+            return collection.Mapto<DataCollection<ClientDTO>>();
+        }
+
+        public async Task<ClientDTO> GetAsync(int id)
+        {
+            return (await _context.Clients.SingleAsync(x => x.ClientId == id)).Mapto<ClientDTO>();
+        }
+    }
+}
