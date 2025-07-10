26dd8bc185ec638781600ceeb835882c4bf891a2
Angel Luna <angellunas2609@gmail.com>
2025-07-09 18:32:03 -0600

    Se agrego modelo, configuracion y context de customer

---
+++ src/Services/Customer/Customer.Persistence.Database/Configuration/ClientConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Customer.Domain;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Customer.Persistence.Database.Configuration
+{
+    public class ClientConfiguration
+    {
+        public ClientConfiguration(EntityTypeBuilder<Client> entityBuiler) 
+        {
+            entityBuiler.HasKey(x => x.ClientId);
+            entityBuiler.Property(x => x.Name).IsRequired().HasMaxLength(100);
+
+            var clients = new List<Client>();
+
+            for(var i = 1; i <= 10; i++)
+            {
+                clients.Add(new Client 
+                {
+                    ClientId = i,
+                    Name = $"Client {i}"
+                });
+            }
+            entityBuiler.HasData(clients);
+        }
+    }
+}
